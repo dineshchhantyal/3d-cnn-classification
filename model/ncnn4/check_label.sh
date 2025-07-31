3788acc79e4cbe8e1e0e5443cba63881e0ce8724
Dinesh Chhantyal <dchhantyal@rusty1.cm.cluster>
2025-07-31 12:48:55 -0400

    Remove deprecated scripts and files from the ncnn4 model directory; update README with example commands for existing and custom datasets.

--- model/ncnn4/check_label.sh
+++
@@ -1,4 +0,0 @@
-find /mnt/home/dchhantyal/3d-cnn-classification/data/nuclei_state_dataset/v3/stable/ \
-  -mindepth 1 -maxdepth 1 -type d \
-  ! -exec bash -c 'test -f "$0/t/binary_label_cropped.tif"' {} \; \
-  -exec rm -r {} +
